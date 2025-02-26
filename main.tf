f829fd93546cbe7cd3f4385cd2fb529b55e43d35
agusikprogramusik <agnieszkagontkowska@o2.pl>
2025-02-26 21:29:20 +0100

    Security groups separation, changes in sec_group traffic

--- main.tf
+++ main.tf
@@ -38,32 +38,3 @@ resource "aws_instance" "my_ec2" {
     replace_triggered_by = [aws_security_group.sec_group]
   }
 }
-
-resource "aws_security_group" "sec_group" {
-  name        = "my_sec_group"
-  description = "Allow inbound traffic"
-}
-
-resource "aws_vpc_security_group_ingress_rule" "allow_ssh_traffic" {
-  security_group_id = aws_security_group.sec_group.id
-  ip_protocol       = "tcp"
-  from_port         = 22
-  to_port           = 22
-  cidr_ipv4         = "0.0.0.0/0"
-}
-
-resource "aws_vpc_security_group_ingress_rule" "allow_all_inbound_traffic" {
-  security_group_id = aws_security_group.sec_group.id
-  ip_protocol       = "-1"
-  from_port         = 0
-  to_port           = 0
-  cidr_ipv4         = "0.0.0.0/0"
-}
-
-resource "aws_vpc_security_group_egress_rule" "allow_all_outbound_traffic" {
-  security_group_id = aws_security_group.sec_group.id
-  ip_protocol       = "-1"
-  from_port         = 0
-  to_port           = 0
-  cidr_ipv4         = "0.0.0.0/0"
-}
